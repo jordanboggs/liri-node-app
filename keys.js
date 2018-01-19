cce794997a6950e2ecfc213e07dbe4043b6a0e23
Jordan Boggs <boggsjp@gmail.com>
2018-01-19 09:53:28 -0700

    Added keys and tokens

---
+++ keys.js
@@ -0,0 +1,11 @@
+/* This would typically be ignored by git, but needed for homework */
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '3RjiR6t1TcDl4g7IAW33RUAcO',
+  consumer_secret: '	k6IAPEQS1nkMRayV61GP2rdbhfggnHGogbTRlP0NpVUBkcXX26',
+  access_token_key: '1287169538-silym7Q2OnEIJlzjt93x6jr6wJc6ZHHhK5Mu7zq',
+  access_token_secret: '	ZYLYcrNGqg1u3euJaI9I2ePO1LBBsCh1j61eWhJm6aSgz',
+}
+
+module.exports = twitterKeys;
